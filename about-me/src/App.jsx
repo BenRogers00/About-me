1a695187816d41fd577dba1f6b6fbf640855cf94
BenRogers00 <benr004@outlook.com>
2026-01-03 18:56:09 +1300

    complete rough layout

--- about-me/src/App.jsx
+++ about-me/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import placeholder from './assets/placeholder_silhouette.png'
 
 function App() {
 
@@ -6,18 +7,24 @@ function App() {
     <>
       <nav>
         <ul>
-          <li><div className='name'>Ben.</div></li>
+          <li><div className='name'><strong style={{fontSize: '1em'}}>Ben.</strong></div></li>
           <li>About Me</li>
           <li>Projects</li>
           <li>Contact</li>
         </ul>
       </nav>
-      <div className='hero_text'>
-        <h1>Hi, I'm Ben Rogers!</h1>
-        <h2>Jr Software Developer</h2>
-      </div>
+      <div className='home-page'>
       <div className='landing'>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis dolor vel augue maximus, vitae placerat urna tempus. Phasellus auctor turpis dolor, non euismod eros consectetur sed. Praesent sed malesuada nulla. Morbi eu lectus malesuada, fringilla diam vitae, finibus mi. Pellentesque habitant morbi</p>
+      <div className='hero_text'>
+        <h1>Hi, I'm <strong>Ben Rogers</strong>!</h1>
+        <h2>Jr. Software Developer</h2>
+      </div>
+      <div className='landing-text'>
+        <p>Motivated and results-driven software development graduate with an academic foundation in software development and cybersecurity. Proven leadership skills through managing teams professionally and within university work, as well as delivering aclient-focused R&D project.</p>
+        <p>I am currently an IT Service Desk Technician for Hallensteins Glassons Holdings, providing support to retail stores and head office users.</p>
+      </div>
+      </div>
+        <img className='landing-image' src={placeholder} alt='Placeholder'/>
       </div>
     </>
   )

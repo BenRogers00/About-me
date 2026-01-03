71ec3c750f5b151ac8a6367746e758bfdde9a1f0
BenRogers00 <benr004@outlook.com>
2026-01-03 16:42:58 +1300

    add basic layout with placeholder text

--- about-me/src/App.jsx
+++ about-me/src/App.jsx
@@ -4,17 +4,20 @@ function App() {
 
   return (
     <>
-    <nav>
-
-      <ul>
-        <li><name>Ben.</name></li>
-        <li>About Me</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-      <div>
-        <h1>About Me</h1>
+      <nav>
+        <ul>
+          <li><div className='name'>Ben.</div></li>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div className='hero_text'>
+        <h1>Hi, I'm Ben Rogers!</h1>
+        <h2>Jr Software Developer</h2>
+      </div>
+      <div className='landing'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis dolor vel augue maximus, vitae placerat urna tempus. Phasellus auctor turpis dolor, non euismod eros consectetur sed. Praesent sed malesuada nulla. Morbi eu lectus malesuada, fringilla diam vitae, finibus mi. Pellentesque habitant morbi</p>
       </div>
     </>
   )
